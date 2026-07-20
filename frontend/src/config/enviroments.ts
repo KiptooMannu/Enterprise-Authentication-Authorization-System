099aa6292f70411c1106bb7a367e713e6888dec2
KiptooMannu <kkgg7241@gmail.com>
2026-07-20 11:30:05 +0300

    Allchanges

--- frontend/src/config/enviroments.ts
+++ frontend/src/config/enviroments.ts
@@ -1,3 +1,3 @@
 export const environments = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://enterprise-authentication-authorization.onrender.com/api',
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api',
 }
